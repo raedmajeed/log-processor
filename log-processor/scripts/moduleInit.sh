457dad92c0e0d8f91d1cb5652b11cb652a32fac2
blacksmithop <angstycoder101@gmaii.com>
2025-03-26 12:31:43 +0530

    feat: Move files

--- log-processor/scripts/moduleInit.sh
+++
@@ -1,7 +0,0 @@
-cd ../
-rm -rf go.mod go.sum
-# go clean -cache
-go mod init LOGProcessor
-go mod tidy
-export SELF_CFG_PATH=${PWD}
-cd -
--- log-processor/scripts/stop_backendService.sh
+++
@@ -1,9 +0,0 @@
-
-cd ../docker || exit 1
-
-docker-compose down
-
-cd -
-
-echo "services stopped succesfully"
-
